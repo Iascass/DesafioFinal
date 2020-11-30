2e1d5a2d4dc012c18ca8dd0ee8d39d1a834e36fe
Iascass <wbef10@gmail.com>
2020-11-30 15:22:11 -0400

    correções3

--- register.js
+++ register.js
@@ -2,5 +2,5 @@ if ('serviceWorker' in navigator) {
  navigator.serviceWorker.register('sw.js');
  .then(function() {
    console.log('serviceWorker está registrado')})
-  catch(function(){console.log('Erro ao registrar serviceWorker')})
+  .catch(function(){console.log('Erro ao registrar serviceWorker')})
 }
